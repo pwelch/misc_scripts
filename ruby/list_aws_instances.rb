0e3abb029e694bc317c9936c4f86eafef6bcc30f
pwelch <git@pwelch.net>
2016-08-12 18:21:12 -0400

    Fix variable

--- ruby/list_aws_instances.rb
+++ ruby/list_aws_instances.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/env ruby
 require 'json'
+require 'csv'
 
-# aws ec2 describe-instances --region=us-east-1 > us-east-1_ec2.json
+# aws ec2 describe-instances --region=us-east-1 > us-east-1.json
 # aws rds describe-db-instances --region=us-east-1 > us-east-1_rds.json
 
 def stringify_tags(array_hash)
@@ -10,25 +11,44 @@ def stringify_tags(array_hash)
 end
 
 def print_info(instances)
-  @rds = true if instances["DBInstances"]
-  if @rds
+  if instances["DBInstances"]
     instances["DBInstances"].each do |rds|
-      puts "Type: #{rds["DBInstanceClass"]} AZ: #{rds["AvailabilityZone"]} ID: #{rds["DBInstanceIdentifier"]}"
+      puts "Type: rds Size: #{rds["DBInstanceClass"]} AZ: #{rds["AvailabilityZone"]} ID: #{rds["DBInstanceIdentifier"]}"
     end
   else # assume EC2
     instances["Reservations"].each do |ec2|
-      puts "Type: #{ec2["Instances"].first["InstanceType"]} AZ: #{ec2["Instances"].first["Placement"]["AvailabilityZone"]} Tags: #{stringify_tags(ec2["Instances"].first["Tags"])}"
+      puts "Type: ec2 Size: #{ec2["Instances"].first["InstanceType"]} AZ: #{ec2["Instances"].first["Placement"]["AvailabilityZone"]} Tags: #{stringify_tags(ec2["Instances"].first["Tags"])}"
     end
   end
 end
 
-file = ARGV[0]
-
-if !File.exist?(file)
-  puts "Unable to load file: #{file}"
-  exit 1
+def write_info(instances, file)
+  CSV.open("file.csv", "a+") do |csv|
+    if instances.has_key?("DBInstances")
+      instances["DBInstances"].each do |rds|
+        csv << [ 'RDS', rds["DBInstanceClass"], rds["AvailabilityZone"], rds["DBInstanceIdentifier"] ]
+      end
+    else # assume EC2
+      instances["Reservations"].each do |ec2|
+        csv << [ 'EC2', ec2["Instances"].first["InstanceType"], ec2["Instances"].first["Placement"]["AvailabilityZone"], stringify_tags(ec2["Instances"].first["Tags"]) ]
+      end
+    end
+  end
 end
 
-list = JSON.parse(File.read(file))
+#file = ARGV[0]
+#
+#if !File.exist?(file)
+#  puts "Unable to load file: #{file}"
+#  exit 1
+#end
 
-print_info(list)
+files = Dir.glob('*.json')
+files.each do |file|
+  list = JSON.parse(File.read(file))
+#   print_info(list)
+  write_info(list, file)
+end
+
+puts "Done."
+
